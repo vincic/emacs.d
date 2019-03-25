ae2ff0fa5af759758518fd4c0f46e75fb1534125
Sasha Vincic <sasha@vincic.org>
2019-03-25 17:31:12 +0100

    typescript: lsp-mode

--- lisp/init-typescript.el
+++ lisp/init-typescript.el
@@ -1,5 +1,5 @@
-(maybe-require-package 'tide)
-(maybe-require-package 'tss)
+;; (maybe-require-package 'tide)
+;; (maybe-require-package 'tss)
 (maybe-require-package 'typescript-mode)
 
 (defun setup-tide-mode ()
@@ -19,13 +19,46 @@
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
+;;(add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 ;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
+;;(add-hook 'before-save-hook 'tide-format-before-save)
 ;; format options
-(setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
+;;(setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
 
+(use-package lsp-mode
+  :custom
+  (lsp-prefer-flymake nil)
+  (lsp-auto-guess-root t)
+  :bind
+  (:map lsp-mode-map
+        ("C-c C-r" . lsp-rename))
+        ([remap typescript-find-symbol] . lsp-goto-implementation))
 
+(use-package lsp-ui
+  :commands
+  (lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-use-webkit nil)
+  (lsp-ui-doc-include-signature t)
+  (lsp-ui-doc-max-width 100)
+  (lsp-ui-doc-max-height 100)
+  (lsp-ui-sideline-enable nil)
+  (lsp-ui-flycheck-enable t)
+  :bind
+  (:map lsp-ui-mode-map
+        ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+        ([remap xref-find-references] . lsp-ui-peek-find-references)))
+
+(use-package typescript-mode
+  :hook
+  (typescript-mode . lsp))
+
+(use-package company-lsp :commands company-lsp)
+
+(use-package projectile
+  :bind
+  (:map projectile-mode-map
+        ("C-c p" . projectile-command-map)))
 
 (provide 'init-typescript)
