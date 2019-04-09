80132e993fc4377ee343330ec60ce51bdaf8f46c
Sasha Vincic <sasha@vincic.org>
2019-04-09 16:43:19 +0200

    recentf: use-package

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -1,7 +1,9 @@
-(recentf-mode 1)
-(setq-default
- recentf-max-saved-items 1000
- recentf-exclude '("/tmp/" "/ssh:"))
-
+(use-package recentf
+  :custom
+  (recentf-case-fold-search t)
+  (recentf-max-saved-items 2000)
+  (recentf-exclude '("/tmp/" "/ssh:"))
+  (recentf-save-file "~/.emacs.d/var/recentf.el"))
 
 (provide 'init-recentf)
+;;; init-recentf.el ends here
